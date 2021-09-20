2a447d43df0d1670e7880285a059e819bbcbe368
vanessuniq <vfotso@mitre.org>
2021-09-19 21:23:33 -0400

    Created useUser hook that will retrieve the active user data from firestore

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,9 +1,15 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import UserContext from "../context/user";
 import { getUserByUserId } from "../services/firebase";
 
 function Sidebar() {
   const { authUser} = useContext(UserContext)
+  useEffect(() => {
+    effect
+    return () => {
+      cleanup
+    }
+  }, [input])
   console.log({docs: getUserByUserId(authUser.uid)})
   return (
     <div className="">
