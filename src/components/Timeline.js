64c425658152fbf9edf2b03f0fb310219ccbe611
vanessuniq <vfotso@mitre.org>
2021-09-27 12:46:08 -0400

    Formatting

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,9 +1,9 @@
 function Timeline() {
- return (
-   <div className="container col-span-2">
-    I am the timeline
-   </div>
- )
+  return (
+    <div className="container col-span-2">
+      I am the timeline
+    </div>
+  )
 };
 
 export default Timeline;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -4,21 +4,21 @@ import Sidebar from "../components/sidebar/Sidebar";
 import Timeline from "../components/Timeline";
 
 function Dashboard() {
- useEffect(() => {
-   document.title = "Instagram";
- }, []);
+  useEffect(() => {
+    document.title = "Instagram";
+  }, []);
 
- return (
-  <div className="bg-gray-background">
-    <Header />
-    <div className="grid grid-cols-3 gap-4 justify-between 
-      max-w-screen-lg mx-auto"
-    >
-      <Timeline />
-      <Sidebar />
+  return (
+    <div className="bg-gray-background">
+      <Header />
+      <div className="grid grid-cols-3 gap-4 justify-between 
+        max-w-screen-lg mx-auto"
+      >
+        <Timeline />
+        <Sidebar />
+      </div>
     </div>
-  </div>
- );
+  );
 };
 
 export default Dashboard;
