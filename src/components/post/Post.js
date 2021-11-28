a4d6ede5826fd8b8194b5ff24f74f5093cd24497
vanessuniq <vfotso@mitre.org>
2021-11-27 20:26:45 -0500

    Single post component draft

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types";
 
 function Post({ post }) {
+  // TODO: Post needs the following components: (header, user, image, actions, footer)
   return (
     <div>
       Single post
@@ -9,7 +10,17 @@ function Post({ post }) {
 };
 
 Post.propTypes = {
-  post: PropTypes.object.isRequired
+  post: PropTypes.shape({
+    docId: PropTypes.string.isRequired,
+    username: PropTypes.string.isRequired,
+    comments: PropTypes.array.isRequired,
+    dateCreated: PropTypes.number.isRequired,
+    currentUserLiked: PropTypes.bool.isRequired,
+    imageSrc: PropTypes.string.isRequired,
+    likes: PropTypes.array.isRequired,
+    photoId: PropTypes.number.isRequired,
+    userId: PropTypes.string.isRequired
+  })
 };
 
 export default Post
